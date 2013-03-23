b2a88bdc1638a967a4ea33fac47a07845bbb0fbf
Ben Gibbs <ben@bobop.co.uk>
2013-03-23 17:00:36 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Busfair::Application.config.secret_token = 'a0097e218a56aa77c22e291d84bc93b8001660bc95a5122e3cda2f6d4dbc901a3e4714177857291d9719b1794e75001225a6dec749f2212b5ef3e7240f95a0ca'
